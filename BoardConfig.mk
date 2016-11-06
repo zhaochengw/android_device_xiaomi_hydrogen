4515152d7e6dd81c6d9dac19784d0aeee83b3ff8
zhaocheng_wei <zhaochengw@qq.com>
2016-11-06 10:05:16 +0800

    add support for helium

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_OTA_ASSERT_DEVICE := hydrogen
+TARGET_OTA_ASSERT_DEVICE := hydrogen,helium
 
 DEVICE_PATH := device/xiaomi/hydrogen
 
